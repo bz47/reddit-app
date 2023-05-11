c7f330dffffe086ac83839ad3e867271445aa59a
b-ez47 <noreply>
2023-05-11 15:29:13 -0400

    add react router

--- src/Components/Reddits/Reddits.js
+++ src/Components/Reddits/Reddits.js
@@ -2,6 +2,23 @@ import React from "react";
 import styles from "./Reddits.module.css";
 
 function Reddits() {
+
+    const redditPosts = async () => {
+        const baseUrl = '';
+        const endpoint = '';
+        const requestParams = '';
+        const urlToFetch = '';
+
+        try {
+            const response = await fetch(urlToFetch);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                const post = jsonResponse. ;
+            }
+        } catch (errors) {
+            alert(errors.message); 
+        }
+    }
     return(
         <div>
             <img className={styles.card}/>
